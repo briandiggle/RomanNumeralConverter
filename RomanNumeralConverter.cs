399d2ae343dc3efa700a089be6900537d16fe756
alexdgarland <alex.d.garland@gmail.com>
2014-04-30 21:57:24 +0100

    Add code! (Converter class, NUnit tests)

---
+++ RomanNumeralConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RomanNumerals
+{
+    class RomanNumeralConverter
+    {
+        Dictionary<int, String> formatList;
+        Dictionary<int, char[]> characterList;
+
+        public RomanNumeralConverter()
+        {
+            formatList = new Dictionary<int, String>    //Gives standard formats which are basically used across all powers of 10
+                {   { 0, "" },
+                    { 1, "{0}" },
+                    { 2, "{0}{0}" },
+                    { 3, "{0}{0}{0}" },
+                    { 4, "{0}{1}" },
+                    { 5, "{1}" },
+                    { 6, "{1}{0}" },
+                    { 7, "{1}{0}{0}" },
+                    { 8, "{1}{0}{0}{0}" },
+                    { 9, "{0}{2}" }
+                };
+
+            characterList = new Dictionary<int, char[]> // Gives the characters representing (the unit), (5x the unit) at a given power of 10
+            {   { 0, new char[] { 'I', 'V' } },
+                { 1, new char[] { 'X', 'L' } },
+                { 2, new char[] { 'C', 'D' } },
+                { 3, new char[] { 'M', '!' } },     // Only the first element of the char array will ever be used
+                { 4, new char[] { '!', '!' } }      // Characters here will never be used - just needed so thousands part can look for the next element up in a consistent way
+            };
+        }
+
+        private String GetNumeralPart(int input, int powerOf10)
+        {
+            String format = formatList[(input/(int)Math.Pow(10, powerOf10)) % 10];
+            return String.Format(format, (characterList[powerOf10])[0], (characterList[powerOf10])[1], (characterList[(powerOf10+1)])[0]);
+        }
+
+        public string GetNumerals(int input)
+        {
+            if (input < 1)  {   throw new ArgumentException("The Romans didn't have zero or negative numbers!");    }
+            if (input > 3888)   {   throw new ArgumentException("Number is too large to be represented in Roman numerals!");    }
+            
+            return Enumerable.Range(0, 4).Aggregate("", (currentString, nextPower) => GetNumeralPart(input, nextPower) + currentString);
+        }
+
+    }
+}
---
+++ Tests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+
+namespace RomanNumerals
+{
+    
+    [TestFixture]
+    class Tests
+    {
+        RomanNumeralConverter rnc;
+        public Tests()  {   rnc = new RomanNumeralConverter();  }
+
+        [Test]
+        public void Input_1_ShouldReturn_I() { Assert.AreEqual("I", rnc.GetNumerals(1)); }
+
+        [Test]
+        public void Input_3_ShouldReturn_III() { Assert.AreEqual("III", rnc.GetNumerals(3)); }
+
+        [Test]
+        public void Input_5_ShouldReturn_V() { Assert.AreEqual("V", rnc.GetNumerals(5)); }
+
+        [Test]
+        public void Input_4_ShouldReturn_IV() { Assert.AreEqual("IV", rnc.GetNumerals(4)); }
+
+        [Test]
+        public void Input_6_ShouldReturn_VI() { Assert.AreEqual("VI", rnc.GetNumerals(6)); }
+
+        [Test]
+        public void Input_7_ShouldReturn_VII() { Assert.AreEqual("VII", rnc.GetNumerals(7)); }
+
+        [Test]
+        public void Input_8_ShouldReturn_VIII() { Assert.AreEqual("VIII", rnc.GetNumerals(8)); }
+
+        [Test]
+        public void Input_9_ShouldReturn_IX() { Assert.AreEqual("IX", rnc.GetNumerals(9)); }
+
+        [Test]
+        public void Input_10_ShouldReturn_X() { Assert.AreEqual("X", rnc.GetNumerals(10)); }
+
+        [Test]
+        public void Input_11_ShouldReturn_XI() { Assert.AreEqual("XI", rnc.GetNumerals(11)); }
+
+        [Test]
+        public void Input_15_ShouldReturn_XV() { Assert.AreEqual("XV", rnc.GetNumerals(15)); }
+
+        [Test]
+        public void Input_35_ShouldReturn_XXXVI() { Assert.AreEqual("XXXVI", rnc.GetNumerals(36)); }
+
+        [Test]
+        public void Input_846_ShouldReturn_DCCCXLVI() { Assert.AreEqual("DCCCXLVI", rnc.GetNumerals(846)); }
+
+        [Test]
+        public void Input_1999_ShouldReturn_MCMXCIX() { Assert.AreEqual("MCMXCIX", rnc.GetNumerals(1999)); }
+
+        [Test]
+        public void Input_2420_ShouldReturn_MMCDXX() { Assert.AreEqual("MMCDXX", rnc.GetNumerals(2420)); }
+        
+    }
+
+}
